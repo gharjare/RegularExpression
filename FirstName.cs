093c1570e9967acca8ad6b2e10529c4a0a8e809a
gharjare <gharjarechetan@gmail.com>
2023-02-14 15:50:14 +0530

    [chetan gharjare] first name is match

---
+++ FirstName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressionProblem
+{
+    public class FirstName
+    {
+        public static string Regex_FirstName = "^[A-Z]{1}[a-z]{2,10}$";
+
+        public bool ValidateName(string name)
+        {
+            return Regex.IsMatch(name, Regex_FirstName);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,20 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regular expression");
+            FirstName firstName = new FirstName();
+            Console.WriteLine("please enter the pattern");
+            string name = Console.ReadLine();
 
+            bool value = firstName.ValidateName(name);
+
+            if (value)
+            {
+                Console.WriteLine("pattern is match");
+            }
+            else
+            {
+                Console.WriteLine("pattern is not match");
+            }
         }
     }
 }
